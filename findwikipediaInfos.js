a6838a8b1f7b910a868732560178d8b0c76aba38
iPPON <ippon1@riseup.net>
2020-06-08 02:34:43 +0200

    div added

--- findwikipediaInfos.js
+++ findwikipediaInfos.js
@@ -1,1 +1,6 @@
 // https://codepen.io/jasonchan/pen/JXwONj
+function loadWikipediaPreview(currentName){
+   // $( "div" ).remove( divid );
+//
+    $("<div class='wikipediainfo'> <p>"+ currentName + "</p> </div>").appendTo('body');
+}
