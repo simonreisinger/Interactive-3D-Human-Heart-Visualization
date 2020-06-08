7fd35ef1d6bc50c8132a9d1c8f3a216cf8cf8b5c
iPPON <ippon1@riseup.net>
2020-06-08 16:08:16 +0200

    source renamed

--- findwikipediaInfos.js
+++ findwikipediaInfos.js
@@ -1,28 +1,29 @@
 // https://codepen.io/jasonchan/pen/JXwONj
 // http://jsfiddle.net/fm0vxg32/
-function loadWikipediaPreview(currentName){
-    //searchTerm = "Posterior_vein_of_the_left_ventricle";
-    var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+ currentName +"&callback=?";
-    //$("<div class='wikipediainfo'> <h3>"+ currentName + "</h3> </div>").appendTo('body');
+function loadWikipediaPreview(currentName) {
+    if (currentName !== null && currentName !== undefined && currentName !== "") {
+        //searchTerm = "Posterior_vein_of_the_left_ventricle";
+        var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + currentName + "&callback=?";
+        //$("<div class='wikipediainfo'> <h3>"+ currentName + "</h3> </div>").appendTo('body');
 
-    $.ajax({
-        type: "GET",
-        url: url,
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
-            console.log(data)
-            var markup = data.parse.text["*"];
-            var blurb = $('<div class=\'wikipediainfo\'></div>').html(markup);
-            //$('#article').html($(blurb).find('wikipediainfo'));
-            $(blurb).appendTo('body');
+        $.ajax({
+            type: "GET",
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            dataType: "json",
+            success: function (data, textStatus, jqXHR) {
+                console.log(data)
+                var markup = data.parse.text["*"];
+                var blurb = $('<div class=\'wikipediainfo\'></div>').html(markup);
+                //$('#article').html($(blurb).find('wikipediainfo'));
+                $(blurb).appendTo('body');
 
-        },
-        error: function (errorMessage) {
-        }
-    });
-
+            },
+            error: function (errorMessage) {
+            }
+        });
+    }
 }
 
 /*
