d0e79c1cf7cc68629f2dc29d2d305d5f5809b77d
iPPON <ippon1@riseup.net>
2020-06-09 22:41:42 +0200

    wikipedia article added

--- findwikipediaInfos.js
+++ findwikipediaInfos.js
@@ -2,9 +2,12 @@
 // http://jsfiddle.net/fm0vxg32/
 function loadWikipediaPreview(currentName) {
     if (currentName !== null && currentName !== undefined && currentName !== "") {
-        //searchTerm = "Posterior_vein_of_the_left_ventricle";
-        var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + currentName + "&callback=?";
-        //$("<div class='wikipediainfo'> <h3>"+ currentName + "</h3> </div>").appendTo('body');
+        let currentWiki =  Postnatal_anatomical_structure.filter(obj => {
+            return obj.Filename.includes(currentName)
+        })[0];
+        //currentWiki = "Posterior_vein_of_the_left_ventricle";
+        console.log(currentWiki)
+        let url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + currentWiki.wikipediaInfos + "&callback=?";
 
         $.ajax({
             type: "GET",
@@ -14,72 +17,13 @@ function loadWikipediaPreview(currentName) {
             dataType: "json",
             success: function (data, textStatus, jqXHR) {
                 console.log(data)
-                var markup = data.parse.text["*"];
-                var blurb = $('<div class=\'wikipediainfo\'></div>').html(markup);
+                let markup = data.parse.text["*"];
+                let blurb = $('<div class=\'wikipediainfo\'></div>').html(markup);
                 //$('#article').html($(blurb).find('wikipediainfo'));
                 $(blurb).appendTo('body');
-
             },
             error: function (errorMessage) {
             }
         });
     }
 }
-
-/*
-$(function() {
-    // enter
-    $("#searchTerm").keypress(function(e){
-        if(e.keyCode===13){
-            var searchTerm = $("#searchTerm").val();
-            var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
-            $.ajax({
-                url: url,
-                type: 'GET',
-                contentType: "application/json; charset=utf-8",
-                async: false,
-                dataType: "json",
-                success: function(data, status, jqXHR) {
-                    //console.log(data);
-                    $("#output").html();
-                    for(var i=0;i<data[1].length;i++){
-                        $("#output").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
-                    }
-
-                }
-            })
-        }
-    });
-// click ajax call
-    $("#search").on("click", function() {
-        var searchTerm = $("#searchTerm").val();
-        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
-        $.ajax({
-            url: url,
-            type: 'GET',
-            contentType: "application/json; charset=utf-8",
-            async: false,
-            dataType: "json",
-            // plop data
-            success: function(data, status, jqXHR) {
-                //console.log(data);
-                $("#output").html();
-                for(var i=0;i<data[1].length;i++){
-                    $("#output").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
-                }
-
-            }
-        })
-            .done(function() {
-                console.log("success");
-            })
-            .fail(function() {
-                console.log("error");
-            })
-            .always(function() {
-                console.log("complete");
-            });
-
-
-    });
-});*/
