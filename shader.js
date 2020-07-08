3f3b1552b9691e87696ba214bf12d1fae7f88b41
iPPON <ippon1@riseup.net>
2020-07-08 12:55:29 +0200

    menu and hover information added

---
+++ shader.js
@@ -0,0 +1,19 @@
+let vertexShader = [
+    "varying vec2 vUv; ",
+    "void main() { ",
+    "   vUv = uv; ",
+    "   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 ); ",
+    "} "
+].join('\n');
+
+let fragmentShader = [
+    "uniform sampler2D baseTexture; ",
+    "uniform sampler2D bloomTexture; ",
+    "varying vec2 vUv; ",
+    "vec4 getTexture( sampler2D texelToLinearTexture ) { ",
+    "   return mapTexelToLinear( texture2D( texelToLinearTexture , vUv ) ); ",
+    "} ",
+    "void main() { ",
+    "   gl_FragColor = ( getTexture( baseTexture ) + vec4( 1.0 ) * getTexture( bloomTexture ) ); ",
+    "} ",
+].join('\n');
